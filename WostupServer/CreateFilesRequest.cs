36582f4e6655e0ab0c1b34540a88b622b5851eeb
bo bo <pierlucaperla@gmail.com>
2026-09-12 20:06:56 +0200

    fix CreatedFilesRequest

--- WostupServer/CreateFilesRequest.cs
+++ WostupServer/CreateFilesRequest.cs
@@ -2,7 +2,7 @@
 {
     public sealed record CreateFilesRequest
     {
-        public required List<IFormFile> Files { get; set; }
+        public required IFormFileCollection Files { get; set; }
         public required string UserName { get; set; }
         public required string UserNumber { get; set; }
         public required string ServerDestinationPath { get; set; }
